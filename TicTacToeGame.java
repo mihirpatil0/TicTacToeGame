0ebf7327cfc2ec6e89a7fc1743919c0c0dfc9d61
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-04 18:36:02 +0530

    Displaying user choice of input

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,15 +1,15 @@
 package game;
 
-import java.util.Iterator;
+import java.util.Scanner;
 
 public class TicTacToeGame 
 {
 	static char[] tttBoard = new char[10];
 	
-	/**
-	 * {@summary}
-	 * This method create board.
-	 */
+	/*This method create board.
+	0th index ignored to make it user friendly.
+	Assigning empty space to blocks.*/
+	 
 	public static void createBoard()
 	{
 		for(int i=1; i<tttBoard.length; i++)
@@ -18,8 +18,35 @@ public class TicTacToeGame
 		}
 	}
 	
+	//Asking user to input X Or O.
+	public static void userInput()
+	{
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the character x or o");
+		char userInput = scanner.next().charAt(0);
+		if( userInput == 'x' || userInput == 'o' )
+		{
+			if(userInput == 'x')
+			{
+				System.out.println("Player chooses x & o is for computer.");
+			}
+			else
+			{
+				System.out.println("Player chooses o & x is for computer.");
+			}
+		}
+		else
+		{
+			System.out.println("Invalid Input ! Try again.");
+			userInput();
+		}
+	}
+	
+	
 	public static void main(String[] args)
 	{
-		
+		//Calling the method.
+		createBoard();
+		userInput();
 	}
 }
