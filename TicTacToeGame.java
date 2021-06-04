841ca23a9a4a5299220d894f2c82d3a77534f868
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-04 17:48:51 +0530

    Added: CreateBoard() function

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,9 +1,25 @@
 package game;
 
+import java.util.Iterator;
+
 public class TicTacToeGame 
 {
+	static char[] tttBoard = new char[10];
+	
+	/**
+	 * {@summary}
+	 * This method create board.
+	 */
+	public static void createBoard()
+	{
+		for(int i=1; i<tttBoard.length; i++)
+		{
+			tttBoard[i] = '-';
+		}
+	}
+	
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Tic-Tac-Toe Game.");
+		
 	}
 }
