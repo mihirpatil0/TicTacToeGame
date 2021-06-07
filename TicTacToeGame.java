dc46b24bd93559d040166dc87768a9174e1bfc52
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-07 18:52:18 +0530

    Checking for winer or tie after every move.

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -8,8 +8,9 @@ public class TicTacToeGame
 {
 	static char[] tictactoeBoard = new char[10];
 	static Scanner scanner;
-	static char userInput;
-
+	static char playerInput;
+	static char computerInput;
+	
 	/**
 	* This method create board. 0th index ignored to make it user friendly.
 	* Assigning empty space to blocks.
@@ -28,26 +29,30 @@ public class TicTacToeGame
 	 * Either x or o.
 	 * vice versa will get assigned.
 	 */
-	public static void userInput() 
+	public static void playerInput() 
 	{
+		
 		scanner = new Scanner(System.in);
 		System.out.println("Choose the character x or o");
-		userInput = scanner.next().charAt(0);
-		if (userInput == 'x' || userInput == 'o') 
+		playerInput = scanner.next().charAt(0);
+		if (playerInput == 'x' || playerInput == 'o') 
 		{
-			if (userInput == 'x') 
+			if (playerInput == 'x') 
 			{
+				computerInput = 'o';
 				System.out.println("Player chooses x & o is assigned to computer.\n");
 			} 
 			else 
 			{
+				computerInput = 'x';
 				System.out.println("Player chooses o & x is assigned to computer.\n");
+				//computerMove();
 			}
 		} 
 		else 
 		{
 			System.out.println("Invalid Input ! Try again.\n");
-			userInput();
+			playerInput();
 		}
 		tossWhoIsFirst();
 	}
@@ -56,7 +61,8 @@ public class TicTacToeGame
 	/**
 	 * Displaying empty tic-tac-toe board.
 	 */
-	static void showBoard() {
+	static void showBoard() 
+	{
 		System.out.println("|---|---|---|");
 		System.out.println("| " + tictactoeBoard[1] + " | " + tictactoeBoard[2] + " | " + tictactoeBoard[3] + " |");
 		System.out.println("|-----------|");
@@ -81,7 +87,7 @@ public class TicTacToeGame
 				tictactoeBoard[i] = Character.forDigit(i, REDIX);
 			}
 		}
-		showBoard();
+		//showBoard();
 	}
 
 	
@@ -89,21 +95,58 @@ public class TicTacToeGame
 	 * Checking with the player selected slot number,if it is empty or not.
 	 * If empty assigning the chosen value in it.
 	 */
-	public static void userMove() 
+	public static void playerMove() 
 	{
-		System.out.println("\nEnter a slot number :\n");
-		int nmb = scanner.nextInt();
+		System.out.println("\nPlayer: Enter a slot number :\n");
+		int boardSlotNumber = scanner.nextInt();
 
-		if (nmb > 0 && nmb <= 9 && tictactoeBoard[nmb] != 'x' && tictactoeBoard[nmb] != 'o')
+		if (boardSlotNumber > 0 && boardSlotNumber <= 9 && tictactoeBoard[boardSlotNumber] != 'x' && tictactoeBoard[boardSlotNumber] != 'o')
 		{
-			tictactoeBoard[nmb] = userInput;
-			beforeMoveBoard();
+			tictactoeBoard[boardSlotNumber] = playerInput;
+			//beforeMoveBoard();
+			showBoard();
 		} 
 		else 
 		{
 			System.out.println("Invalid input");
-			userMove();
+			playerMove();
 		}
+	}
+	
+	/**
+	 * UC - 8
+	 * If computer wins coin toss.
+	 * computer will assign the value either x or o inside board. 
+	 */
+	public static void computerMove() 
+	{
+		Random random = new Random();
+		int comupterRandomNumber = random.nextInt(9);
+		if(comupterRandomNumber == 1)
+		{
+			tictactoeBoard[1] = computerInput;
+			//beforeMoveBoard();
+		}
+		else if(comupterRandomNumber == 3)
+		{
+			tictactoeBoard[3] = computerInput;
+			//beforeMoveBoard(); 
+		}
+		else if(comupterRandomNumber == 7)
+		{
+			tictactoeBoard[7] = computerInput;
+			//beforeMoveBoard(); 
+		}
+		else if(comupterRandomNumber == 9)
+		{
+			tictactoeBoard[9] = computerInput;
+			//beforeMoveBoard();
+		}
+		else
+		{
+			computerMove();
+		}
+		beforeMoveBoard();
 	}
 
 	/**
@@ -126,10 +169,12 @@ public class TicTacToeGame
 			if(playerHeadOrTail == generatedHeadOrTail)
 			{
 				System.out.println("Player won toss ! Make first move.");
+				beforeMoveBoard();
 			}
 			else 
 			{
-				System.out.println("Computer won toss ! Make first move.");
+				System.out.println("Computer won toss ! Make's first move.");
+				computerMove();
 			}
 		}
 		else
@@ -138,14 +183,53 @@ public class TicTacToeGame
 			tossWhoIsFirst();
 		}
 	}
+	
+	/**
+	 * This function will check either winning or tie condition after every move.
+	 * If any condition satisfied stop the game and print winner name.
+	 */
+	private static void winnerOrTie()
+	{
+		//Check each row for winning or tie condition.
+		if(tictactoeBoard[0] == tictactoeBoard[1] && tictactoeBoard[1] == tictactoeBoard[2] && tictactoeBoard[0] != 1)
+		{
+			System.out.println("Winner");
+		}
+		if(tictactoeBoard[3] == tictactoeBoard[4] && tictactoeBoard[4] == tictactoeBoard[5] && tictactoeBoard[3] != 4)
+		{
+			System.out.println("Winner");
+		}
+		if(tictactoeBoard[6] == tictactoeBoard[7] && tictactoeBoard[7] == tictactoeBoard[8] && tictactoeBoard[6] != 7)
+		{
+			System.out.println("Winner");
+		}
+		//Check diagonally for winning or tie condition.
+		if(tictactoeBoard[0] == tictactoeBoard[4] && tictactoeBoard[4] == tictactoeBoard[8] && tictactoeBoard[0] != 1)
+		{
+			System.out.println("Winner");
+		}
+		if(tictactoeBoard[6] == tictactoeBoard[4] && tictactoeBoard[4] == tictactoeBoard[2] && tictactoeBoard[6] != 7)
+		{
+			System.out.println("Winner");
+		}
+		else
+		{
+			//nobody has won yet.
+			//changeTurn();
+		}
+	}
 		
+	/**
+	 * @param args
+	 * Calling static methods.
+	 */
 	public static void main(String[] args) 
 	{
 		createBoard();
-		userInput();
+		playerInput();
 		showBoard();
-		beforeMoveBoard();
-		userMove();
+		playerMove();
+		winnerOrTie();
 	}
 
 }
